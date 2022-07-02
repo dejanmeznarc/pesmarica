3e7db7b5da59b6d2757b159dab568667c819f9bc
dejan <dejan@meznarc.si>
2022-07-02 11:24:06 +0200

    Sort index in slovenian alphabet

--- tools/generatePdf.sh
+++ tools/generatePdf.sh
@@ -1,6 +1,8 @@
 script_path=$PWD
 
+export SONGIDX_LOCALE=sl_SI.UTF-8
 
+echo "locale set to ${SONGIDX_LOCALE}"
 
 echo "[generatePdf.sh] changing to source directory"
 cd ../src || exit 2
@@ -11,7 +13,7 @@ echo "[generatePdf.sh] Compiling list of songs"
 pdflatex -file-line-error -interaction=nonstopmode -synctex=1  -output-format=pdf -output-directory=../out main.tex
 
 echo "[generatePdf.sh] Building index"
-texlua $script_path/songidx.lua ../out/titleidx.sxd ../out/titleidx.sbx || exit 5
+texlua $script_path/songidx.lua -l sl_SI.UTF-8 ../out/titleidx.sxd ../out/titleidx.sbx || exit 5
 echo "[generatePdf.sh] Generating pdf file"
 
 pdflatex -file-line-error -interaction=nonstopmode -synctex=1  -output-format=pdf -output-directory=../out main.tex
